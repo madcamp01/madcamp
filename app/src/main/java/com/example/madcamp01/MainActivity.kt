fa24b5f56da14ab24a7d3cf0d75e9a2489a32c2e
[jieunyy] <ina011011@naver.com>
2024-06-27 16:21:04 +0900

    feat: 탭 구현 완료

--- app/src/main/java/com/example/madcamp01/MainActivity.kt
+++ app/src/main/java/com/example/madcamp01/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.madcamp01
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        // TabLayout과 ViewPager2를 레이아웃에서 찾음
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+
+        // ViewPager2에 어댑터를 설정
+        val adapter = ViewPagerAdapter(this)
+        viewPager.adapter = adapter
+
+        // TabLayout과 ViewPager2를 연결
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Tab 1"
+                1 -> "Tab 2"
+                2 -> "Tab 3"
+                else -> null
+            }
+        }.attach()
     }
 }
--- app/src/main/java/com/example/madcamp01/second/SecondFragment.kt
+++ app/src/main/java/com/example/madcamp01/second/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.madcamp01.first
+package com.example.madcamp01.second
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.madcamp01.R
 
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_second, container, false)
     }
 }
