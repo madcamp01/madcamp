8112e389875cb276c0fa0b67cc0c2dcf4e6339b2
banna117 <banna111222@gmail.com>
2024-06-29 11:42:51 +0900

    fix: db 수정

--- app/src/main/java/com/example/madcamp01/DB/Entities/ContactReviewCrossRef.kt
+++ app/src/main/java/com/example/madcamp01/DB/Entities/ContactReviewCrossRef.kt
@@ -3,7 +3,7 @@ package com.example.madcamp01.DB.Entities
 import androidx.room.Entity
 
 @Entity(primaryKeys = ["personId", "reviewId"])
-data class ContactReviewCrossRef(
+data class ContactReviewCrossRef(/*personId가 쓴 리뷰들*/
     val personId: Int,
     val reviewId: Int
 )
