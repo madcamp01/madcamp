5be6dc90eb2401e538f20984d6ac0470be175dcf
banna117 <banna111222@gmail.com>
2024-07-01 20:08:58 +0900

    feat: imageoverlay 추가

--- app/src/main/java/com/example/madcamp01/first/ReviewAdapterProfile.kt
+++ app/src/main/java/com/example/madcamp01/first/ReviewAdapterProfile.kt
@@ -3,7 +3,7 @@ package com.example.madcamp01.first
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madcamp01.R
@@ -15,6 +15,8 @@ class ReviewAdapterProfile(
     private val isEditMode: Boolean
 ) : RecyclerView.Adapter<ReviewAdapterProfile.ReviewViewHolder>() {
 
+    private val selectedReviews = mutableSetOf<Review>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.profile_item_review, parent, false)
         return ReviewViewHolder(view)
@@ -24,12 +26,16 @@ class ReviewAdapterProfile(
         val review = reviews[position]
         holder.bind(review)
         if (isEditMode) {
-            holder.deleteButton.visibility = View.VISIBLE
-            holder.deleteButton.setOnClickListener {
-                onDeleteClick(review)
+            holder.selectCheckBox.visibility = View.VISIBLE
+            holder.selectCheckBox.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    selectedReviews.add(review)
+                } else {
+                    selectedReviews.remove(review)
+                }
             }
         } else {
-            holder.deleteButton.visibility = View.GONE
+            holder.selectCheckBox.visibility = View.GONE
         }
     }
 
@@ -40,11 +46,15 @@ class ReviewAdapterProfile(
         notifyDataSetChanged()
     }
 
+    fun getSelectedReviews(): List<Review> {
+        return selectedReviews.toList()
+    }
+
     class ReviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val reviewComment: TextView = itemView.findViewById(R.id.commentTextView)
         private val reviewRating: TextView = itemView.findViewById(R.id.ratingTextView)
         private val reviewDate: TextView = itemView.findViewById(R.id.dateTextView)
-        val deleteButton: Button = itemView.findViewById(R.id.delete_button)
+        val selectCheckBox: CheckBox = itemView.findViewById(R.id.select_checkbox)
 
         fun bind(review: Review) {
             reviewComment.text = review.comment
