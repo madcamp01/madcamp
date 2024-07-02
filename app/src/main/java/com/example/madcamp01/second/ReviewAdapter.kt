0c92807369528bbd4c7e47a8a46b24e3d3f8f655
banna117 <banna111222@gmail.com>
2024-07-02 11:14:55 +0900

    feat: 탭2 리뷰에서 프로필 상세페이지 띄우기

--- app/src/main/java/com/example/madcamp01/second/ReviewAdapter.kt
+++ app/src/main/java/com/example/madcamp01/second/ReviewAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.madcamp01.second
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.madcamp01.ContactDetailActivity
 import com.example.madcamp01.DB.AppDatabase
 import com.example.madcamp01.DB.DAO.ContactDao
 import com.example.madcamp01.DB.Entities.Contact
@@ -34,6 +37,18 @@ class ReviewAdapter(private val reviews: List<Review>) : RecyclerView.Adapter<Re
                 ratingTextView.text = "Rating: ${review.rating}"
                 commentTextView.text = review.comment
                 dateTextView.text = review.date
+
+                profilePictureImageView.setOnClickListener {
+                    val context = itemView.context
+                    val intent = Intent(context, ContactDetailActivity::class.java).apply {
+                        putExtra("CONTACT_ID", contact.personId)
+                    }
+                    context.startActivity(intent)
+                }
+
+                Log.d("adapter", review.rating.toString())
+                Log.d("adapter", review.comment)
+                Log.d("adapter", review.date)
             }
         }
     }
--- app/src/main/java/com/example/madcamp01/third/ThirdFragment.kt
+++ app/src/main/java/com/example/madcamp01/third/ThirdFragment.kt
@@ -27,50 +27,10 @@ class ThirdFragment : Fragment() {
 
         // MapView 초기화
         mapView = view.findViewById(R.id.map_view)
-// MapView 초기화 및 콜백 설정
-        mapView.start(object : KakaoMapReadyCallback {
-            override fun onMapReady(kakaoMap: KakaoMap) {
-                // 인증 후 API가 정상적으로 실행될 때 호출됨
-            }
 
-            override fun getPosition(): LatLng {
-                // 지도 시작 시 위치 좌표를 설정
-                return LatLng.from(37.406960, 127.115587)
-            }
-
-            override fun getZoomLevel(): Int {
-                // 지도 시작 시 확대/축소 줌 레벨 설정
-                return 15
-            }
-
-            override fun getMapViewInfo(): MapViewInfo {
-                // 지도 시작 시 App 및 MapType 설정
-                return MapViewInfo.from(MapType.NORMAL.toString())
-            }
-
-            override fun getViewName(): String {
-                // KakaoMap의 고유한 이름을 설정
-                return "MyFirstMap"
-            }
-
-            override fun isVisible(): Boolean {
-                // 지도 시작 시 visible 여부를 설정
-                return true
-            }
-
-            override fun getTag(): String {
-                // KakaoMap의 tag을 설정
-                return "FirstMapTag"
-            }
-        })
 
         return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        // MapView 정리
-        mapView.onSurfaceDestroyed()
-    }
 
 }
